035fb369b6e4120f43e90998cee6d5a23894a0f3
Shakalniy <itsmy6328@gmail.com>
2024-07-22 22:28:43 +0600

    refactor: домашний виджет вынесен в отдельный файл

---
+++ lib/views/main_menu_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MainMenuView extends StatelessWidget {
+  const MainMenuView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+
+}
