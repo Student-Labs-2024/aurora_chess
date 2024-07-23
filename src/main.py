0bf05e513eece6a9e1e5e253d89761cfd6783c0f
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-07-24 00:45:17 +0300

    chore: добавлен файл для конфигурации

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
-
 import uvicorn
 
+from core.config import settings
 
 app = FastAPI()
 
 
 if __name__ == "__main__":
+    uvicorn.run(app="main:app", reload=True)
     uvicorn.run(
-        "main:app")
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
