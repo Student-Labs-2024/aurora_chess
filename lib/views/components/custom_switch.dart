cd27af1950affd229aa23d28429bb45423fc91e8
Shakalniy <itsmy6328@gmail.com>
2024-07-23 22:55:14 +0600

    feat: доработка дизайна переключателя

--- lib/views/components/custom_switch.dart
+++ lib/views/components/custom_switch.dart
@@ -1,30 +1,55 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../exports.dart';
-
-class CustomSwitch extends StatelessWidget {
+class CustomSwitch extends StatefulWidget {
   const CustomSwitch({super.key, required this.onChanged});
-  final void Function() onChanged;
+  final void Function(bool) onChanged;
+
+  @override
+  State<CustomSwitch> createState() => _CustomSwitchState();
+}
+
+class _CustomSwitchState extends State<CustomSwitch> {
+
+  bool isToggle = false;
+  String activeIcon = "assets/images/icons/dark_switch.svg";
+  String inactiveIcon = "assets/images/icons/light_switch.svg";
+
+  void onToggle() {
+    setState(() {
+      isToggle = !isToggle;
+    });
+    widget.onChanged(isToggle);
+  }
 
   @override
   Widget build(BuildContext context) {
-    var provider = Provider.of<ThemeProvider>(context, listen: false);
     var scheme = Theme.of(context).colorScheme;
-    return SizedBox(
-      width: 90,
-      height: 40,
-      child: FittedBox(
-        fit: BoxFit.fill,
-        child: CupertinoSwitch(
-          value: provider.isDarkMode,
-          onChanged: (value) {
-            onChanged();
-          }
+    return GestureDetector(
+      onTap: onToggle,
+      child: Container(
+        height: 40,
+        width: 90,
+        padding: const EdgeInsets.all(3),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24),
+          color: scheme.secondaryContainer,
         ),
+        child: AnimatedAlign(
+          alignment: isToggle ? Alignment.centerRight : Alignment.centerLeft,
+          duration: const Duration(milliseconds: 150),
+          child: Container(
+            width: 34,
+            height: 34,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white
+            ),
+            child: SvgPicture.asset(isToggle ? activeIcon : inactiveIcon),
+          ),
+        ),
+
       ),
     );
   }
-
 }
