799735ced9a8bf15a6f3684fd96553079e40d87b
shakal <itsmy6328@gmail.com>
2024-07-24 17:49:25 +0600

    feat: из главной страницы убрана кнопка сетевой игры

--- lib/views/main_menu_view.dart
+++ lib/views/main_menu_view.dart
@@ -13,9 +13,9 @@ class MainMenuView extends StatefulWidget {
 class _MainMenuViewState extends State<MainMenuView> {
 
   var textStyles = const TextStyles();
-  final String slogan = "Побеждать в шахматах — побеждать в жизни";
-  final String netButton = "Сетевая игра";
-  final String localButton = "Локальная игра";
+  final slogan = "Побеждать в шахматах — побеждать в жизни";
+  final netButton = "Сетевая игра";
+  final localButton = "Локальная игра";
   String piecesFile = "";
 
 
@@ -34,74 +34,74 @@ class _MainMenuViewState extends State<MainMenuView> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: scheme.background,
-        body: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    CustomSwitch(
-                      onChanged: (value) {
-                        setState(() {
-                          provider.toggleTheme();
-                          piecesFile = provider.isDarkMode ? "pieces_dark" : "pieces_light";
-                        });
-                      },
-                    ),
-                    ButtonToGuide(
-                      backGroundColor: scheme.onPrimaryContainer,
-                      iconColor: ColorsConst.neutralColor0,
-                      height: 32,
-                      width: 32,
-                      iconSize: 18,
-                      onTap: () {},
-                    ),
-                  ],
-                ),
-                Stack(
-                  children: [
-                    Container(
-                      margin: const EdgeInsets.only(right: 40, top: 20),
-                      child: Text(
-                        slogan,
-                        style: textStyles.title1,
+        body: ConstrainedBox(
+          constraints: BoxConstraints(
+              minWidth: MediaQuery.of(context).size.width,
+              minHeight: MediaQuery.of(context).size.height
+          ),
+          child: IntrinsicHeight(
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomSwitch(
+                        onChanged: (value) {
+                          setState(() {
+                            provider.toggleTheme();
+                            piecesFile = provider.isDarkMode ? "pieces_dark" : "pieces_light";
+                          });
+                        },
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 190),
-                      child: Container(
-                        alignment: Alignment.centerRight,
-                        child: SvgPicture.asset("assets/images/icons/$piecesFile.svg"),
+                      ButtonToGuide(
+                        backGroundColor: scheme.onPrimaryContainer,
+                        iconColor: ColorsConst.neutralColor0,
+                        height: 32,
+                        width: 32,
+                        iconSize: 18,
+                        onTap: () {},
                       ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 30,),
-                NextPageButton(
-                  text: netButton,
-                  textColor: scheme.background,
-                  buttonColor: scheme.onSecondaryContainer,
-                  isClickable: false,
-                  onTap: () {},
-                ),
-                const SizedBox(height: 24,),
-                NextPageButton(
-                  text: localButton,
-                  textColor: ColorsConst.primaryColor0,
-                  buttonColor: scheme.secondaryContainer,
-                  isClickable: true,
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (BuildContext context) {
-                        return const GameSettingsView();
-                      })
-                    );
-                  },
-                ),
-              ],
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 40, top: 20),
+                        child: Text(
+                          slogan,
+                          style: textStyles.title1,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 190),
+                        child: Container(
+                          alignment: Alignment.centerRight,
+                          child: SvgPicture.asset("assets/images/icons/$piecesFile.svg"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30,),
+                  const SizedBox(height: 60,),
+                  const SizedBox(height: 24,),
+                  NextPageButton(
+                    text: localButton,
+                    textColor: ColorsConst.primaryColor0,
+                    buttonColor: scheme.secondaryContainer,
+                    isClickable: true,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (BuildContext context) {
+                          return const GameSettingsView();
+                        })
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
