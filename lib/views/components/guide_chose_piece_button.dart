2d391a8f1b8842b0e062adfddf5d6ee746600d7f
Shakalniy <itsmy6328@gmail.com>
2024-07-25 17:51:07 +0600

    fix: убран не используемый импорт

--- lib/views/components/guide_chose_piece_button.dart
+++ lib/views/components/guide_chose_piece_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:frontend/constants/colors.dart';
 
 class GuideChosePieceButton extends StatelessWidget {
   const GuideChosePieceButton({
