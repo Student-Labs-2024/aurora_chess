9689b5522237e44c4f9bf18c9bae1a4bc860968a
Shakalniy <itsmy6328@gmail.com>
2024-07-30 17:27:30 +0600

    feat: создан виджет загрузки экрана

--- lib/views/components/components.dart
+++ lib/views/components/components.dart
@@ -1,3 +1,4 @@
 export 'next_page_button.dart';
 export 'button_to_guide.dart';
 export 'custom_icon_button.dart';
+export 'loading_widget.dart';
---
+++ lib/views/components/loading_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/constants/colors.dart';
+
+class LoadingWidget extends StatelessWidget {
+  const LoadingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var scheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      backgroundColor: scheme.background,
+      body: const Center(
+        child: CircularProgressIndicator(
+          color: ColorsConst.primaryColor200,
+        ),
+      ),
+    );
+  }
+
+}
