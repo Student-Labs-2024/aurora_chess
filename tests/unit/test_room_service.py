ee26511ba136f9c08997d6599e93733121c181de
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-08-01 17:39:57 +0300

    refactor: добавлен репозиторий для RoomService, удалены фабрики для создания комнаты, подкорректированы тесты для RoomService

--- tests/unit/test_room_service.py
+++ tests/unit/test_room_service.py
@@ -1,40 +1,39 @@
 import unittest
-from unittest.mock import MagicMock, patch
-from core.entities.AbstractGameRoom import AbstractGameRoom
-from core.factories.AbstractGameRoomFactory import AbstractGameRoomFactory
+from unittest.mock import MagicMock
+from core.repository.RoomRepository.AbstractRoomRepository import AbstractRoomRepository
 from core.services.RoomService import RoomService
 
 
 class TestRoomService(unittest.TestCase):
 
     def setUp(self):
-        self.game_room_factory = MagicMock(spec=AbstractGameRoomFactory)
-        self.room_service = RoomService(self.game_room_factory)
+        self.game_room_repository = MagicMock(spec=AbstractRoomRepository)
+        self.room_service = RoomService(self.game_room_repository)
 
     def test_create_room(self):
         room_name = "Test Room"
         game_type = "Test Game"
+        self.game_room_repository.get_room.return_value = None
+        self.game_room_repository.create_room.return_value = None
         self.room_service.create_room(room_name, game_type)
-        self.game_room_factory.create_room.assert_called_once_with(room_name, game_type)
-        self.assertIn(room_name, self.room_service.rooms)
+        self.game_room_repository.create_room.assert_called_once_with(room_name, game_type)
+        self.game_room_repository.get_room.assert_called_once_with(room_name)
 
     def test_get_room(self):
         room_name = "Test Room"
-        game_room = MagicMock(spec=AbstractGameRoom)
-        self.room_service.rooms[room_name] = game_room
-        self.assertEqual(self.room_service.get_room(room_name), game_room)
+        self.room_service.get_room(room_name)
+        self.game_room_repository.get_room.assert_called_once_with(room_name)
 
     def test_get_room_not_found(self):
         room_name = "Non-existent Room"
+        self.game_room_repository.get_room.return_value = None
         self.assertIsNone(self.room_service.get_room(room_name))
 
     def test_add_player_to_room(self):
         room_name = "Test Room"
         player = MagicMock()
-        game_room = MagicMock(spec=AbstractGameRoom)
-        self.room_service.rooms[room_name] = game_room
         self.room_service.add_player_to_room(room_name, player)
-        game_room.add_player.assert_called_once_with(player)
+        self.game_room_repository.add_player_to_room.assert_called_once_with(room_name ,player)
 
     def test_add_player_to_non_existent_room(self):
         room_name = "Non-existent Room"
