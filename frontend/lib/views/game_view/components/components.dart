78cad75724f733d65073037ca10d436d536c6c7f
Dimon4o <55238214+Dimon4o@users.noreply.github.com>
2024-08-09 23:00:44 +0600

    refactor: виджет имени и преимущества вынесен в отдельный файл

--- frontend/lib/views/game_view/components/components.dart
+++ frontend/lib/views/game_view/components/components.dart
@@ -2,3 +2,4 @@ export "chess_board_widget.dart";
 export "game_info_and_controls.dart";
 export "game_info_and_controls/game_info_and_controls.dart";
 export 'piece_choose_window.dart';
+export 'name_with_advantage_widget.dart';
