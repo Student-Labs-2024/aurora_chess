707ea3cebc3c5e3f827493021555c3cdd3dff32d
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-08-11 23:16:55 +0300

    refactor: удалены фабрики по созданию сессий

--- backend/src/core/factories/websocket_player_session_factory.py
+++
@@ -1,7 +0,0 @@
-from core.entities.player_session.websocket_player_session import WebsocketPlayerSession
-from core.factories.abstract_player_session_factory import PlayerSessionFactory
-
-
-class WebsocketPlayerSessionFactory(PlayerSessionFactory):
-    def create_session(self, *args) -> WebsocketPlayerSession:
-        return WebsocketPlayerSession(*args)
