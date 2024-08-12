857a09377a67d4c55e15790a0d7c8950c757828b
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-08-12 21:54:48 +0300

    refactor: добавлен обработчик в зависимость

--- backend/src/dependencies.py
+++ backend/src/dependencies.py
@@ -1,5 +1,6 @@
 from core.factories.chess_player_factory import ChessPlayerFactory
 from core.handlers.connection_request_handler import ConnectionRequestHandler
+from core.handlers.player_move_request_handler import PlayerMoveRequestHandler
 from core.handlers.room_init_request_handler import RoomInitRequestHandler
 from core.repository.room_repository.ram_room_repository import RAMRoomRepository
 from core.services.message_handler_service import MessageHandlerService
@@ -15,4 +16,9 @@ message_dispatcher.register_handler(
 message_dispatcher.register_handler(
     "roomConnectionRequest",
     ConnectionRequestHandler(rooms_service, player_session_factory),
+)
+
+message_dispatcher.register_handler(
+    "playerMoveRequest",
+    PlayerMoveRequestHandler(room_service=rooms_service),
 )
