7d5942a361b430818235ae311a407326df78cfa2
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-08-12 21:54:12 +0300

    chore: написан обработчик для ходов, поступающих на сервер, а также тесты для него

---
+++ backend/src/core/handlers/player_move_request_handler.py
@@ -0,0 +1,108 @@
+import asyncio
+import json
+
+from core.entities.game_room.abstract_game_room import AbstractGameRoom
+from core.entities.player.chess_player import ChessPlayer
+from core.entities.player_session.abstract_player_session import AbstractPlayerSession
+from core.handlers.message_handler import MessageHandler
+from core.schemas.player_move_request import PlayerMoveRequest
+from core.schemas.player_move_response import PlayerMoveResponse, PlayerMoveResponseData
+from core.services.room_service import RoomService
+from utils.chess_board import interpritation_move, fen_to_list_board
+
+
+class PlayerMoveRequestHandler(MessageHandler):
+    def __init__(
+        self,
+        room_service: RoomService,
+    ):
+        self.__room_service: RoomService = room_service
+
+    def __is_valid_player(
+        self, room: AbstractGameRoom, player, player_session: AbstractPlayerSession
+    ):
+        players: list[ChessPlayer] = room.get_players()
+        for room_player in players:
+            if (
+                room_player.get_name() == player.player_name
+                and room_player.get_side() == player.player_side
+                and room_player.get_session() is player_session
+            ):
+                return True
+        return False
+
+    def handle(self, *args) -> json:
+        message = PlayerMoveRequest.model_validate(args[0])
+        player_session: AbstractPlayerSession = args[1]
+
+        data = message.data
+
+        game_type = data.game_type
+        room_name = data.room_name
+        player_info = data.player
+        player_move = data.player_move
+
+        chess_move = interpritation_move(player_move, player_info.player_side)
+
+        room: AbstractGameRoom = self.__room_service.get_room(room_name)
+
+        if not (
+            room
+            and room.is_active()
+            and room.get_game_type() == game_type
+            and self.__is_valid_player(room, player_info, player_session)
+            and room.is_legal_move(chess_move)
+        ):
+            response_data = PlayerMoveResponseData(
+                confirmationStatus="not valid",
+                player=data.player,
+                player_move=data.player_move,
+                gameType=game_type,
+                roomName=room_name,
+            )
+            response_message = PlayerMoveResponse(
+                jsonType="moveConfirmationResponce", data=response_data
+            )
+            loop = asyncio.get_event_loop()
+            loop.create_task(
+                player_session.send_message(
+                    json.dumps(response_message.model_dump(by_alias=True))
+                )
+            )
+            return
+
+        status, new_board_fen, game_status = room.make_move(chess_move)
+        response_data = PlayerMoveResponseData(
+            confirmationStatus="confirmed",
+            player=data.player,
+            player_move=data.player_move,
+            gameType=game_type,
+            gameStatus=game_status,
+            roomName=room_name,
+        )
+        response_message = PlayerMoveResponse(
+            jsonType="moveConfirmationResponce", data=response_data
+        )
+        response_message.data.player_move.board = fen_to_list_board(new_board_fen)
+        to_other_player_message = PlayerMoveResponse(
+            jsonType="enemysMove", data=response_data
+        )
+        other_player_session = [
+            player.get_session()
+            for player in room.get_players()
+            if player.get_session() != player_session
+        ][0]
+
+        loop = asyncio.get_event_loop()
+        loop.create_task(
+            player_session.send_message(
+                json.dumps(response_message.model_dump(by_alias=True))
+            )
+        )
+
+        loop = asyncio.get_event_loop()
+        loop.create_task(
+            other_player_session.send_message(
+                json.dumps(to_other_player_message.model_dump(by_alias=True))
+            )
+        )
---
+++ backend/tests/unit/test_player_move_request_handler.py
@@ -0,0 +1,440 @@
+import asyncio
+import json
+import unittest
+from unittest.mock import MagicMock, AsyncMock
+
+from core.engines.chess_engine import ChessEngine
+from core.entities.game_room.abstract_game_room import AbstractGameRoom
+from core.entities.player.chess_player import ChessPlayer
+from core.handlers.player_move_request_handler import (
+    PlayerMoveRequestHandler,
+)
+from core.schemas.player_move_response import PlayerMoveResponseData, PlayerMoveResponse
+from core.services.room_service import RoomService
+
+
+class TestPlayerMoveRequestHandler(unittest.IsolatedAsyncioTestCase):
+    async def test_valid_move(self):
+        room_service = MagicMock(spec=RoomService)
+        game_room = MagicMock(spec=AbstractGameRoom)
+        game_room.make_move.return_value = (
+            True,
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+            "game in progress",
+        )
+        room_service.get_room.return_value = game_room
+        player_session = MagicMock()
+        player_session.send_message = AsyncMock()
+
+        handler = PlayerMoveRequestHandler(room_service)
+
+        player_room = MagicMock(ChessPlayer)
+        player_room.get_session.return_value = player_session
+        player_room.get_name.return_value = "player_white"
+        player_room.get_side.return_value = "white"
+        session_other_player = MagicMock()
+        session_other_player.send_message = AsyncMock()
+        other_player = MagicMock(ChessPlayer)
+        other_player.name = "player_black"
+        other_player.side = "black"
+        other_player.get_session.return_value = session_other_player
+
+        game_type = "test_invalid_session"
+        game_room.get_game_type.return_value = game_type
+        game_room.is_legal_move.return_value = True
+        game_room.get_players.return_value = [player_room, other_player]
+
+        room_name = "invalid_test_session"
+        player = {"playerName": "player_white", "playerSide": "white"}
+        player_move = {
+            "board": [
+                ["r", "n", "b", "q", "k", "b", "n", "r"],
+                ["p", "p", "p", "p", "p", "p", "p", "p"],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ],
+            "piece": "P",
+            "source": {"column": 0, "row": 6},
+            "destination": {"column": 0, "row": 5},
+        }
+        message = {
+            "jsonType": "playerMoveRequest",
+            "data": {
+                "gameType": game_type,
+                "roomName": room_name,
+                "player": player,
+                "player_move": player_move,
+            },
+        }
+
+        handler.handle(message, player_session)
+        await asyncio.sleep(0.1)
+
+        response_data = PlayerMoveResponseData(
+            confirmationStatus="confirmed",
+            player=player,
+            player_move=player_move,
+            gameType=game_type,
+            roomName=room_name,
+            gameStatus="game in progress",
+        )
+        response_message = PlayerMoveResponse(
+            jsonType="moveConfirmationResponce", data=response_data
+        )
+
+        room_service.get_room.assert_called_once_with(room_name)
+        game_room.get_game_type.asser_called_once()
+        game_room.get_players.asser_called_once()
+        game_room.is_legal_move.assert_called_once()
+        player_room.get_session.asser_called_once()
+        player_session.send_message.assert_called_once_with(
+            json.dumps(response_message.model_dump(by_alias=True))
+        )
+
+    async def test_invalid_move_board(self):
+        room_service = MagicMock(spec=RoomService)
+        game_room = MagicMock(spec=AbstractGameRoom)
+        room_service.get_room.return_value = game_room
+        player_session = MagicMock()
+        player_session.send_message = AsyncMock()
+
+        handler = PlayerMoveRequestHandler(room_service)
+
+        player_room = MagicMock(ChessPlayer)
+        player_room.get_session.return_value = player_session
+        player_room.get_name.return_value = "player_white"
+        player_room.get_side.return_value = "white"
+
+        game_type = "test_invalid_session"
+        game_room.get_game_type.return_value = game_type
+        game_room.is_legal_move.return_value = (
+            "r1bqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+            == "rrbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+        )
+        game_room.get_players.return_value = [player_room]
+
+        room_name = "invalid_test_session"
+        player = {"playerName": "player_white", "playerSide": "white"}
+        player_move = {
+            "board": [
+                ["r", "", "b", "q", "k", "b", "n", "r"],
+                ["p", "p", "p", "p", "p", "p", "p", "p"],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ],
+            "piece": "P",
+            "source": {"column": 0, "row": 6},
+            "destination": {"column": 0, "row": 5},
+        }
+        message = {
+            "jsonType": "playerMoveRequest",
+            "data": {
+                "gameType": game_type,
+                "roomName": room_name,
+                "player": player,
+                "player_move": player_move,
+            },
+        }
+
+        handler.handle(message, player_session)
+        await asyncio.sleep(0.1)
+
+        response_data = PlayerMoveResponseData(
+            confirmationStatus="not valid",
+            player=player,
+            player_move=player_move,
+            gameType=game_type,
+            roomName=room_name,
+        )
+        response_message = PlayerMoveResponse(
+            jsonType="moveConfirmationResponce", data=response_data
+        )
+
+        room_service.get_room.assert_called_once_with(room_name)
+        game_room.get_game_type.asser_called_once()
+        game_room.get_players.asser_called_once()
+        game_room.is_legal_move.assert_called_once()
+        player_room.get_session.asser_called_once()
+        player_session.send_message.assert_called_once_with(
+            json.dumps(response_message.model_dump(by_alias=True))
+        )
+
+    async def test_invalid_move_session(self):
+        room_service = MagicMock(spec=RoomService)
+        game_room = MagicMock(spec=AbstractGameRoom)
+        room_service.get_room.return_value = game_room
+        player_session = MagicMock()
+        player_session.send_message = AsyncMock()
+
+        handler = PlayerMoveRequestHandler(room_service)
+
+        player_room = MagicMock(ChessPlayer)
+        player_room.get_session.return_value = MagicMock()
+        player_room.name = "player_white"
+        player_room.side = "white"
+
+        game_type = "test_invalid_session"
+        game_room.get_game_type.return_value = game_type
+        game_room.is_legal_move.return_value = lambda x: ChessEngine().is_legal_move(x)
+        game_room.get_players.return_value = [player_room]
+
+        room_name = "invalid_test_session"
+        player = {"playerName": "player_white", "playerSide": "white"}
+        player_move = {
+            "board": [
+                ["r", "n", "b", "q", "k", "b", "n", "r"],
+                ["p", "p", "p", "p", "p", "p", "p", "p"],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ],
+            "piece": "P",
+            "source": {"column": 0, "row": 7},
+            "destination": {"column": 0, "row": 7},
+        }
+        message = {
+            "jsonType": "playerMoveRequest",
+            "data": {
+                "gameType": game_type,
+                "roomName": room_name,
+                "player": player,
+                "player_move": player_move,
+            },
+        }
+
+        handler.handle(message, player_session)
+        await asyncio.sleep(0.1)
+
+        response_data = PlayerMoveResponseData(
+            confirmationStatus="not valid",
+            player=player,
+            player_move=player_move,
+            gameType=game_type,
+            roomName=room_name,
+        )
+        response_message = PlayerMoveResponse(
+            jsonType="moveConfirmationResponce", data=response_data
+        )
+
+        room_service.get_room.assert_called_once_with(room_name)
+        game_room.get_game_type.asser_called_once()
+        game_room.get_players.asser_called_once()
+        player_room.get_session.asser_called_once()
+        player_session.send_message.assert_called_once_with(
+            json.dumps(response_message.model_dump(by_alias=True))
+        )
+
+    async def test_invalid_move_player(self):
+        room_service = MagicMock(spec=RoomService)
+        game_room = MagicMock(spec=AbstractGameRoom)
+        room_service.get_room.return_value = game_room
+        player_session = MagicMock()
+        player_session.send_message = AsyncMock()
+
+        handler = PlayerMoveRequestHandler(room_service)
+
+        player_room = MagicMock(ChessPlayer)
+        player_room.get_session.return_value = player_session
+        player_room.name = "player_whitee"
+        player_room.side = "white"
+
+        game_type = "test_invalid_session"
+        game_room.get_game_type.return_value = game_type
+        game_room.is_legal_move.return_value = True
+        game_room.get_players.return_value = [player_room]
+
+        room_name = "invalid_test_session"
+        player = {"playerName": "player_white", "playerSide": "white"}
+        player_move = {
+            "board": [
+                ["r", "n", "b", "q", "k", "b", "n", "r"],
+                ["p", "p", "p", "p", "p", "p", "p", "p"],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ],
+            "piece": "P",
+            "source": {"column": 0, "row": 6},
+            "destination": {"column": 0, "row": 5},
+        }
+        message = {
+            "jsonType": "playerMoveRequest",
+            "data": {
+                "gameType": game_type,
+                "roomName": room_name,
+                "player": player,
+                "player_move": player_move,
+            },
+        }
+
+        handler.handle(message, player_session)
+        await asyncio.sleep(0.1)
+
+        response_data = PlayerMoveResponseData(
+            confirmationStatus="not valid",
+            player=player,
+            player_move=player_move,
+            gameType=game_type,
+            roomName=room_name,
+        )
+        response_message = PlayerMoveResponse(
+            jsonType="moveConfirmationResponce", data=response_data
+        )
+
+        room_service.get_room.assert_called_once_with(room_name)
+        game_room.get_game_type.asser_called_once()
+        game_room.get_players.asser_called_once()
+        player_room.get_session.asser_called_once()
+        player_session.send_message.assert_called_once_with(
+            json.dumps(response_message.model_dump(by_alias=True))
+        )
+
+    async def test_invalid_move_room(self):
+        room_service = MagicMock(spec=RoomService)
+        game_room = MagicMock(spec=AbstractGameRoom)
+        room_service.get_room.return_value = None
+        player_session = MagicMock()
+        player_session.send_message = AsyncMock()
+
+        handler = PlayerMoveRequestHandler(room_service)
+
+        player_room = MagicMock(ChessPlayer)
+        player_room.get_session.return_value = player_session
+        player_room.name = "player_white"
+        player_room.side = "white"
+
+        game_type = "test_invalid_session"
+        game_room.get_game_type.return_value = game_type
+        game_room.is_legal_move.return_value = True
+        game_room.get_players.return_value = [player_room]
+
+        room_name = "invalid_test_session"
+        player = {"playerName": "player_white", "playerSide": "white"}
+        player_move = {
+            "board": [
+                ["r", "n", "b", "q", "k", "b", "n", "r"],
+                ["p", "p", "p", "p", "p", "p", "p", "p"],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ],
+            "piece": "P",
+            "source": {"column": 0, "row": 6},
+            "destination": {"column": 0, "row": 5},
+        }
+        message = {
+            "jsonType": "playerMoveRequest",
+            "data": {
+                "gameType": game_type,
+                "roomName": room_name,
+                "player": player,
+                "player_move": player_move,
+            },
+        }
+
+        handler.handle(message, player_session)
+        await asyncio.sleep(0.1)
+
+        response_data = PlayerMoveResponseData(
+            confirmationStatus="not valid",
+            player=player,
+            player_move=player_move,
+            gameType=game_type,
+            roomName=room_name,
+        )
+        response_message = PlayerMoveResponse(
+            jsonType="moveConfirmationResponce", data=response_data
+        )
+
+        room_service.get_room.assert_called_once_with(room_name)
+        game_room.get_game_type.asser_called_once()
+        game_room.get_players.asser_called_once()
+        player_room.get_session.asser_called_once()
+        player_session.send_message.assert_called_once_with(
+            json.dumps(response_message.model_dump(by_alias=True))
+        )
+
+    async def test_invalid_move_game_type(self):
+        room_service = MagicMock(spec=RoomService)
+        game_room = MagicMock(spec=AbstractGameRoom)
+        room_service.get_room.return_value = game_room
+        player_session = MagicMock()
+        player_session.send_message = AsyncMock()
+
+        handler = PlayerMoveRequestHandler(room_service)
+
+        player_room = MagicMock(ChessPlayer)
+        player_room.get_session.return_value = player_session
+        player_room.name = "player_white"
+        player_room.side = "white"
+
+        game_type = "test_invalid_session"
+        game_room.get_game_type.return_value = game_type + "fvs"
+        game_room.is_legal_move.return_value = True
+        game_room.get_players.return_value = [player_room]
+
+        room_name = "invalid_test_session"
+        player = {"playerName": "player_white", "playerSide": "white"}
+        player_move = {
+            "board": [
+                ["r", "n", "b", "q", "k", "b", "n", "r"],
+                ["p", "p", "p", "p", "p", "p", "p", "p"],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["", "", "", "", "", "", "", ""],
+                ["P", "P", "P", "P", "P", "P", "P", "P"],
+                ["R", "N", "B", "Q", "K", "B", "N", "R"],
+            ],
+            "piece": "P",
+            "source": {"column": 0, "row": 6},
+            "destination": {"column": 0, "row": 5},
+        }
+        message = {
+            "jsonType": "playerMoveRequest",
+            "data": {
+                "gameType": game_type,
+                "roomName": room_name,
+                "player": player,
+                "player_move": player_move,
+            },
+        }
+
+        handler.handle(message, player_session)
+        await asyncio.sleep(0.1)
+
+        response_data = PlayerMoveResponseData(
+            confirmationStatus="not valid",
+            player=player,
+            player_move=player_move,
+            gameType=game_type,
+            roomName=room_name,
+        )
+        response_message = PlayerMoveResponse(
+            jsonType="moveConfirmationResponce", data=response_data
+        )
+
+        room_service.get_room.assert_called_once_with(room_name)
+        game_room.get_game_type.asser_called_once()
+        game_room.get_players.asser_called_once()
+        player_room.get_session.asser_called_once()
+        player_session.send_message.assert_called_once_with(
+            json.dumps(response_message.model_dump(by_alias=True))
+        )
