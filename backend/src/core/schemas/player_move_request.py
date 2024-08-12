9f95ea6e308a4b2691fb7902d481d47618c26138
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-08-12 21:53:14 +0300

    chore: добавлены схемы для запроса с ходом и ответа на него

---
+++ backend/src/core/schemas/player_move_request.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+from core.schemas.player import Player
+from core.schemas.player_move import PlayerMove
+from core.schemas.mixins.external_data_mixin import ExternalDataMixin
+from core.schemas.mixins.json_type_str_mixin import JsonTypeStrMixin
+
+
+class PlayerMoveRequestData(BaseModel):
+    game_type: str = Field(alias="gameType")
+    room_name: str = Field(alias="roomName")
+    player: Player
+    player_move: PlayerMove
+
+
+class PlayerMoveRequest(JsonTypeStrMixin, ExternalDataMixin):
+    data: PlayerMoveRequestData
---
+++ backend/src/core/schemas/player_move_response.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+
+from core.schemas.player import Player
+from core.schemas.player_move import PlayerMove
+from core.schemas.mixins.error_mixin import ErrorsMixin
+from core.schemas.mixins.json_type_str_mixin import JsonTypeStrMixin
+from core.schemas.mixins.system_message_mixin import SystemMessagesMixin
+from core.schemas.room import Room
+
+
+class PlayerMoveResponseData(BaseModel):
+    confirmation_status: str = Field(alias="confirmationStatus")
+    player: Player
+    player_move: PlayerMove
+    game_status: str | None = Field(alias="gameStatus", default=None)
+    game_type: str = Field(alias="gameType")
+    room_name: str = Field(alias="roomName")
+
+
+class PlayerMoveResponse(JsonTypeStrMixin, SystemMessagesMixin, ErrorsMixin):
+    data: PlayerMoveResponseData
