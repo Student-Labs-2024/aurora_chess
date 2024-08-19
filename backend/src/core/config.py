4af8451fca1a01fe7b282aba90e620e18255e66b
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-08-19 13:37:51 +0600

    chore: добавлен конфиг для сервера

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -1,5 +1,5 @@
-from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel, PostgresDsn
 
 
 class RunConfig(BaseModel):
@@ -12,9 +12,24 @@ class ApiPrefix(BaseModel):
     chess: str = "/chess"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=("env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__"
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
