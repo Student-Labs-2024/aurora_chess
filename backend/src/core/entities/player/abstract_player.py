ffb89a533ebe6366f700348c42a504a558cb59e5
Nikolay Koren <nikolai.koren2004@gmail.com>
2024-08-25 22:16:17 +0600

    refactor: добавлены новые поля в player

--- backend/src/core/entities/player/abstract_player.py
+++ backend/src/core/entities/player/abstract_player.py
@@ -2,8 +2,11 @@ from abc import ABC
 
 
 class AbstractPlayer(ABC):
-    def __init__(self, player_session):
+    def __init__(self, player_session, name, side, user_id):
         self.player_session = player_session
+        self.name = name
+        self.side = side
+        self.user_id = user_id
 
     def get_session(self):
         return self.player_session
--- backend/src/core/entities/player/chess_player.py
+++ backend/src/core/entities/player/chess_player.py
@@ -4,10 +4,20 @@ from core.entities.player.abstract_player import AbstractPlayer
 
 
 class ChessPlayer(AbstractPlayer):
-    def __init__(self, player_session, name, side: Literal["white", "black"]):
-        super().__init__(player_session)
-        self.name = name
-        self.side = side
+    def __init__(
+        self,
+        player_session,
+        name,
+        side: Literal["white", "black"],
+        user_id,
+        user_elo=1000,
+        user_age=18,
+        user_games=30,
+    ):
+        super().__init__(player_session, name, side, user_id=user_id)
+        self.user_elo = user_elo
+        self.user_age = user_age
+        self.user_game = user_games
 
     def get_name(self):
         return self.name
